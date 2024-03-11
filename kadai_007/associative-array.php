7185ed5b6bf0e9d4f1ca0904a6c7934b157fb3b8
Naoto Tanzawa <154235099+NaotoTanzawa@users.noreply.github.com>
2024-03-11 20:51:04 +0900

    課題  連想配列を作って値とキーを出力しよう　を提出します

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lamg="ja">
+    <head>
+        <meta charaset="UTF-8">
+        <title>課題：連想配列を作って値とキーを出力しよう</title>
+    </head>
+
+    <body>
+        <?php
+            // 連想配列に名前、値段、重さを代入する
+            $goods = ['name' => 'onion','price' => 200,'weight' => 160];
+
+            // 連想配列の中身を表示する
+            print_r($goods);
+        ?>
+    </body>
+
+</html>
